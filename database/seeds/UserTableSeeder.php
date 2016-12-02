04a4fef2d410e0845170a41b6a2d3b7f50e89b4d
Marcos Mousquere <mmousquere@gmail.com>
2016-12-02 14:30:55 -0300

    Faker y seed

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(User::class)->create([
+            'name' => 'Marcos Mousquere',
+            'gender' => 'm',
+            'password' => bcrypt('1234'),
+        ]);
+        factory(User::class, 119)->create();
+    }
+}
